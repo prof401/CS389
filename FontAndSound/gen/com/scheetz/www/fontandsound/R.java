143be643337302209a881c157934cb5485796580
gscheetz <gscheetz@carroll.edu>
2015-02-02 22:27:57 -0700

    add images

--- FontAndSound/gen/com/scheetz/www/fontandsound/R.java
+++ FontAndSound/gen/com/scheetz/www/fontandsound/R.java
@@ -21,7 +21,9 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int airplane=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int tyronelarge=0x7f020002;
     }
     public static final class id {
         public static final int action_settings=0x7f080002;
@@ -36,7 +38,9 @@ public final class R {
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
+        public static final int airplane=0x7f050005;
         public static final int app_name=0x7f050000;
+        public static final int haiku=0x7f050006;
         public static final int hello1=0x7f050003;
         public static final int hello2=0x7f050004;
         public static final int hello_world=0x7f050002;
