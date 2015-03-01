f6a1fe048aeb56f99f68091906b931bfadc2378f
Gerald Scheetz <bits@april1.net>
2015-03-01 14:24:07 -0700

    create landscape layout

--- NasaDailyImage/gen/com/example/gscheetz/www/nasadailyimage/R.java
+++ NasaDailyImage/gen/com/example/gscheetz/www/nasadailyimage/R.java
@@ -25,11 +25,13 @@ public final class R {
         public static final int nasaimage=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
+        public static final int action_settings=0x7f080006;
         public static final int imageDate=0x7f080001;
         public static final int imageDescription=0x7f080003;
         public static final int imageDisplay=0x7f080002;
         public static final int imageTitle=0x7f080000;
+        public static final int refreshButton=0x7f080004;
+        public static final int setWallpaperButton=0x7f080005;
     }
     public static final class layout {
         public static final int activity_nasa_daily_image=0x7f030000;
