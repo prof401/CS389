0311c7f9ff63bca8e6222ab99918518756682694
gscheetz <gscheetz@carroll.edu>
2015-03-26 10:43:58 -0600

    Initial commit

---
+++ Gerry Mystic Game/src/net/april1/mystic/GameActivity.java
@@ -0,0 +1,31 @@
+package net.april1.mystic;
+
+import net.april1.gerryrunnerapp.R;
+import net.april1.gerryrunnerapp.TimeTrackerAdapter;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.GridView;
+import android.widget.ListView;
+
+public class GameActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+        
+		GridView gridView = (GridView) findViewById(R.id.mystic_grid);
+		timeTrackerAdapter = new TimeTrackerAdapter();
+		listView.setAdapter(timeTrackerAdapter);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.game, menu);
+        return true;
+    }
+    
+}
---
+++ Gerry Mystic Game/src/net/april1/mystic/MysticGame.java
@@ -0,0 +1,22 @@
+package net.april1.mystic;
+
+public class MysticGame {
+	private int rowCount = 4;
+	private int columnCount = 4;
+
+	public int getRowCount() {
+		return rowCount;
+	}
+
+	public void setRowCount(int rowCount) {
+		this.rowCount = rowCount;
+	}
+
+	public int getColumnCount() {
+		return columnCount;
+	}
+
+	public void setColumnCount(int columnCount) {
+		this.columnCount = columnCount;
+	}
+}
