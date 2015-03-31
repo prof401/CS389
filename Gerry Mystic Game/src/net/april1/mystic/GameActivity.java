38dfe5b7d64a263c03e11ace16204748fcac7c3e
Gerald Scheetz <bits@april1.net>
2015-03-30 21:52:36 -0600

    first part of game done

--- Gerry Mystic Game/src/net/april1/mystic/GameActivity.java
+++ Gerry Mystic Game/src/net/april1/mystic/GameActivity.java
@@ -1,9 +1,8 @@
 package net.april1.mystic;
 
-import net.april1.gerryrunnerapp.R;
-import net.april1.gerryrunnerapp.TimeTrackerAdapter;
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 import android.widget.GridView;
 import android.widget.ListView;
@@ -12,14 +11,44 @@ public class GameActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+    	Log.d(this.getClass().getName(), ">>onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
         
-		GridView gridView = (GridView) findViewById(R.id.mystic_grid);
-		timeTrackerAdapter = new TimeTrackerAdapter();
-		listView.setAdapter(timeTrackerAdapter);
+//		GridView gridView = (GridView) findViewById(R.id.mystic_grid);
     }
 
+
+    protected void onStart() {
+    	super.onStart();
+    	Log.d(this.getClass().getName(), ">>onStart");
+	}
+
+    protected void onRestart() {
+    	super.onRestart();
+    	Log.d(this.getClass().getName(), ">>onRestart");
+	}
+
+    protected void onResume() {
+    	super.onResume();
+    	Log.d(this.getClass().getName(), ">>onResume");
+	}
+
+    protected void onPause() {
+    	super.onPause();
+    	Log.d(this.getClass().getName(), ">>onPause");
+	}
+
+    protected void onStop() {
+    	super.onStop();
+    	Log.d(this.getClass().getName(), ">>onStop");
+	}
+
+    protected void onDestroy() {
+    	super.onDestroy();
+    	Log.d(this.getClass().getName(), ">>onDestroy");
+	}
+    
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- Gerry Mystic Game/src/net/april1/mystic/MysticGame.java
+++ Gerry Mystic Game/src/net/april1/mystic/MysticGame.java
@@ -1,8 +1,73 @@
 package net.april1.mystic;
 
+import java.util.Random;
+
 public class MysticGame {
 	private int rowCount = 4;
 	private int columnCount = 4;
+	private Cell[][] board;
+
+	public void startGame() {
+		final Random random = new Random();
+		board = new Cell[rowCount][columnCount];
+		for (int row = 0; row < rowCount; row++) {
+			for (int column = 0; column < columnCount; column++) {
+				Cell newCell = new Cell(row, column, (row * columnCount)
+						+ column);
+				board[row][column] = newCell;
+			}
+		}
+
+		// make one random cell blank
+		int blankRow = random.nextInt(rowCount);
+		int blankColumn = random.nextInt(columnCount);
+		board[blankRow][blankColumn].setFace(null);
+
+		// Shuffle
+		for (int r = 0; r < rowCount; r++) {
+			for (int c = 0; c < columnCount; c++) {
+				exchangeTiles(r, c, random.nextInt(rowCount),
+						random.nextInt(columnCount));
+			}
+		}
+	}
+
+	public boolean isGameWon() {
+		boolean gameWon = true;
+		for (int row = 0; row < rowCount; row++) {
+			for (int column = 0; column < columnCount; column++) {
+				gameWon &= board[row][column].isInFinalPosition(row, column);
+			}
+		}
+		return gameWon;
+	}
+
+	public boolean move(int row, int column) {
+		// check if blank was selected to move
+		// return false as do not know what cell to exchange with
+		if (board[row][column].isBlank())
+			return false;
+
+		final int rowDelta[] = { -1, 0, 0, 1 };
+		final int columnDelta[] = { 0, -1, 1, 0 };
+
+		for (int deltaIndex = 0; deltaIndex < rowDelta.length; deltaIndex++) {
+			if (board[row + rowDelta[deltaIndex]][column
+					+ columnDelta[deltaIndex]].isBlank()) {
+				exchangeTiles(row, column, row + rowDelta[deltaIndex], column
+						+ columnDelta[deltaIndex]);
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	private void exchangeTiles(int r1, int c1, int r2, int c2) {
+		Cell temp = board[r1][c1];
+		board[r1][c1] = board[r2][c2];
+		board[r2][c2] = temp;
+	}
 
 	public int getRowCount() {
 		return rowCount;
@@ -19,4 +84,34 @@ public class MysticGame {
 	public void setColumnCount(int columnCount) {
 		this.columnCount = columnCount;
 	}
+
+	private class Cell {
+		private int startRow; // row of final position
+		private int startColumn; // col of final position
+		private String face;
+
+		public Cell(int startRow, int startColumn, int face) {
+			this.startRow = startRow;
+			this.startColumn = startColumn;
+			this.face = Integer.toString(face);
+		}
+
+		public boolean isInFinalPosition(int row, int column) {
+			return row == startRow && column == startColumn;
+		}
+
+		public String getFace() {
+			return face;
+		}
+
+		public void setFace(String newFace) {
+			this.face = newFace;
+		}
+
+		public boolean isBlank() {
+			return (face == null);
+		}
+
+	}
+
 }
